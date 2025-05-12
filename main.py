a38411b90bf8a13cc1a096161d108f5be2871260
acuomo <acuomo94@gmail.com>
2025-05-11 22:53:02 -0400

    added a bunch of stuff, collision, shooting, splitting of asteroids

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 from constants import *
 from player import *
-from circleshape import *
+import sys
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -9,8 +12,14 @@ def main():
     clock = pygame.time.Clock()
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = updateable
+    Shot.containers = (shots, updateable, drawable)
+    asteroid_field = AsteroidField()
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
@@ -21,6 +30,16 @@ def main():
                 return
         
         updateable.update(dt)
+
+        for asteroid in asteroids: 
+            if asteroid.collides_with(player):
+                print("Game Over")
+                sys.exit()
+
+            for shot in shots: 
+                if asteroid.collides_with(shot):
+                    asteroid.split()
+                    shot.kill()
 
         
         screen.fill("black")
